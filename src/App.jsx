cd0cdd127d61e7f837a467f43e4b0787831bfb22
shubham9829ss <122727957+shubham9829ss@users.noreply.github.com>
2024-03-29 15:29:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,15 +19,12 @@ function App() {
 
   return (
     <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
-      <h1 className="text-3xl text-center">Om Namah Shivay</h1>
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            {/* themebtn*/}
             <ThemeBtn />
           </div>
           <div className="w-full max-w-sm mx-auto">
-            {/* card*/}
             <Card />
           </div>
         </div>
