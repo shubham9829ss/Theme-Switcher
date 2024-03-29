5c54a918b083f25f63fa051af084f58554ad8547
shubham9829ss <shubhamsahu9829@gmail.com>
2024-03-29 15:23:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { ThemeProvider } from "./context/theme";
+import ThemeBtn from "./component/ThemeBtn";
+import Card from "./component/Card";
+
+function App() {
+  const [themeMode, setThemeMode] = useState("light");
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
+  return (
+    <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+      <h1 className="text-3xl text-center">Om Namah Shivay</h1>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            {/* themebtn*/}
+            <ThemeBtn />
+          </div>
+          <div className="w-full max-w-sm mx-auto">
+            {/* card*/}
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
